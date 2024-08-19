8c976d15d9d9bba24793d6d1e647e777c408ec97
Neeraj Samtani <neerajjsamtani@gmail.com>
2024-08-18 17:12:11 -0700

    Add TypeScript config and make CategoryFilter in tsx

---
+++ client/src/CategoryFilter.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Form, InputGroup } from 'react-bootstrap';
+
+// Define a constant array for categories
+const CATEGORIES = [
+  'All',
+  'Alcohol',
+  'Dining',
+  'Entertainment',
+  'Forma',
+  'Groceries',
+  'Hobbies',
+  'Income',
+  'Investment',
+  'Rent',
+  'Shopping',
+  'Subscription',
+  'Transfer',
+  'Transit',
+  'Travel'
+] as const;
+
+// Infer the Category type from the CATEGORIES array
+type Category = typeof CATEGORIES[number];
+
+export default function CategoryFilter({ category, setCategory }: { category: Category, setCategory: (category: Category) => void }) {
+
+  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(event.target.value as Category);
+  }
+
+  return (
+    <>
+      <InputGroup>
+        <InputGroup.Text>Category</InputGroup.Text>
+        <Form.Group controlId="exampleForm.SelectCustom">
+          <Form.Select value={category} onChange={handleCategoryChange}>
+            {CATEGORIES.map(cat => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </InputGroup>
+    </>
+  );
+}
