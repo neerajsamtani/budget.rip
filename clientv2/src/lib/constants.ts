a421c2c194f3cf140cc1ad5fbe65f77bb45f0136
Neeraj Samtani <neerajjsamtani@gmail.com>
2024-08-24 18:57:32 -0700

    Add month and year filter

---
+++ clientv2/src/lib/constants.ts
@@ -0,0 +1,39 @@
+export const CATEGORIES = [
+    'All',
+    'Alcohol',
+    'Dining',
+    'Entertainment',
+    'Forma',
+    'Groceries',
+    'Hobbies',
+    'Income',
+    'Investment',
+    'Rent',
+    'Shopping',
+    'Subscription',
+    'Transfer',
+    'Transit',
+    'Travel'
+] as const;
+
+export const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+    'All'
+] as const;
+
+export const YEARS = [
+    '2022',
+    '2023',
+    '2024'
+] as const;
