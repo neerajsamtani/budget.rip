3e748b77e1f773a73964283cc197684e7a955c02
Neeraj Samtani <neerajjsamtani@gmail.com>
2024-11-17 13:48:49 -0800

    Pass in supabase client to fetch data

--- clientv2/src/lib/serverData.ts
+++ clientv2/src/lib/serverData.ts
@@ -1,8 +1,7 @@
-import { createClient } from '@/utils/supabase/server'
+import { SupabaseClient } from '@supabase/supabase-js';
 
-export const getLineItemsToReview = async () => {
-    const supabase = createClient()
-    const { data: line_items, error } = await supabase
+export const getLineItemsToReview = async (supabaseClient: SupabaseClient) => {
+    const { data: line_items, error } = await supabaseClient
         .from('line_items_to_review')
         .select('date,amount,description,responsible_party,payment_method,id')
     if (error) {
@@ -12,9 +11,8 @@ export const getLineItemsToReview = async () => {
     return line_items;
 };
 
-export const getNetEarningsPerMonth = async () => {
-    const supabase = createClient()
-    const { data, error } = await supabase
+export const getNetEarningsPerMonth = async (supabaseClient: SupabaseClient) => {
+    const { data, error } = await supabaseClient
         .from('net_earnings_per_month')
         .select('*')
     if (error) {
@@ -24,13 +22,21 @@ export const getNetEarningsPerMonth = async () => {
     return data;
 };
 
-export const getAmountPerCategoryPerMonth = async () => {
-    const supabase = createClient()
-    const { data, error } = await supabase
+export const getAmountPerCategoryPerMonth = async (supabaseClient: SupabaseClient) => {
+    const { data, error } = await supabaseClient
         .from('amount_per_category_per_month')
         .select('*')
     if (error) {
         console.error("Error fetching AmountPerCategoryPerMonth:", error);
+        throw error; // Re-throw the error for handling in the caller function
+    }
+    return data;
+};
+
+export const getEvents = async (supabaseClient: SupabaseClient) => {
+    const { data, error } = await supabaseClient.from('events').select('*')
+    if (error) {
+        console.error("Error fetching Events:", error);
         throw error; // Re-throw the error for handling in the caller function
     }
     return data;
