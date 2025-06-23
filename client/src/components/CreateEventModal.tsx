5be1b48953d28b5b948c039acbdebedead2c5236
Neeraj Samtani <neerajjsamtani@gmail.com>
2025-06-22 18:19:21 -0700

    Add tests for CreateEventModal

--- client/src/components/CreateEventModal.tsx
+++ client/src/components/CreateEventModal.tsx
@@ -195,8 +195,8 @@ export default function CreateEventModal({ show, onHide }: { show: boolean, onHi
               />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Label>Override Date:</Form.Label>
-              <Form.Control type={date.type} value={date.value} onChange={date.onChange} />
+              <Form.Label htmlFor="override-date-input">Override Date:</Form.Label>
+              <Form.Control id="override-date-input" type={date.type} value={date.value} onChange={date.onChange} />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Check checked={isDuplicateTransaction.value} onChange={() => isDuplicateTransaction.setCustomValue(!isDuplicateTransaction.value)}
